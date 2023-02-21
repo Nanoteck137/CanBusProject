709f798193d351eb69db0cc8aafd4a3ca087609c
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-02-21 14:29:46 +0100

    Tusk rust bindings

---
+++ tusk-rs/build.rs
@@ -0,0 +1,20 @@
+fn main() {
+    println!("cargo:rerun-if-changed=../tusk.cpp");
+    println!("cargo:rerun-if-changed=../tusk.h");
+
+    cc::Build::new()
+        .cpp(true)
+        .file("../tusk.cpp")
+        .compile("libtusk.a");
+
+    let bindings = bindgen::Builder::default()
+        .clang_args(["-x", "c++"])
+        .header("wrapper.h")
+        .generate()
+        .expect("Failed to generate bindings");
+
+    let out_path = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Failed to write the generated bindings");
+}
---
+++ tusk-rs/src/lib.rs
@@ -0,0 +1,76 @@
+// extern "C" {
+//     fn tusk_get_encode_size(data_length: usize) -> usize;
+//     fn tusk_encode(
+//         input_buffer: *const u8,
+//         input_length: usize,
+//         output: *mut u8,
+//     );
+//     fn tusk_decode(
+//         input_buffer: *const u8,
+//         input_length: usize,
+//         output: *mut u8,
+//     );
+//
+//     fn tusk_checksum(buffer: *const u8, length: usize) -> u16;
+//     fn tusk_check_bytes(checksum: u16, a: *mut u8, b: *mut u8);
+// }
+
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+fn encode(input: &[u8]) -> Vec<u8> {
+    println!("Input: {:x?}", input);
+    let mut buffer = vec![0; input.len() + 1];
+    unsafe { tusk_encode(input.as_ptr(), input.len(), buffer.as_mut_ptr()) };
+
+    buffer
+}
+
+fn decode(input: &[u8]) -> Vec<u8> {
+    let mut buffer = vec![0; 6];
+    unsafe { tusk_decode(input.as_ptr(), input.len(), buffer.as_mut_ptr()) };
+
+    buffer
+}
+
+fn checksum(input: &[u8]) -> u16 {
+    unsafe { tusk_checksum(input.as_ptr(), input.len()) }
+}
+
+fn check_bytes(checksum: u16) -> (u8, u8) {
+    let mut a: u8 = 0;
+    let mut b: u8 = 0;
+    unsafe {
+        tusk_check_bytes(checksum, &mut a, &mut b);
+    }
+
+    (a, b)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn lel() {
+        let data = [1, 2, 0, 4, 3];
+        let result = encode(&data);
+        println!("Result: {:x?}", result);
+
+        let decode_result = decode(&result);
+        println!("Decode Result: {:x?}", decode_result);
+
+        let sum = checksum(&['a' as u8, 'b' as u8, 'c' as u8, 'd' as u8]);
+        println!("Checksum: {:#x}", sum);
+
+        let check = check_bytes(sum);
+        println!("Check Bytes: {:x?}", check);
+
+        let sum = checksum(&[
+            'a' as u8, 'b' as u8, 'c' as u8, 'd' as u8, check.0, check.1,
+        ]);
+        println!("Checksum: {:#x}", sum);
+    }
+}
