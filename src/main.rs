fe40ca18378448020ddda451a9d4761410a5045e
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-02-27 00:45:53 +0100

    Working on the build system

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,42 @@
-struct MainController {}
+use std::path::{Path, PathBuf};
+use std::process::Command;
 
-struct Controller {
-    name: String,
+fn generate_cmake_project<P>(target: P, name: &str, path: P)
+where
+    P: AsRef<Path>,
+{
+    let source = path.as_ref();
+    let target = target.as_ref();
+
+    // cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -S source -B target
+    let status = Command::new("cmake")
+        .arg("-DCMAKE_EXPORT_COMPILE_COMMANDS=1")
+        .arg("-S")
+        .arg(source)
+        .arg("-B")
+        .arg(target)
+        .arg("-G")
+        .arg("Unix Makefiles")
+        .status()
+        .unwrap();
+    println!("Test: {:?}", status);
+}
+
+fn compile_with_make<P>(path: P)
+where
+    P: AsRef<Path>,
+{
+    let path = path.as_ref();
+
+    let status = Command::new("make").arg("-C").arg(path).status().unwrap();
+    println!("Status: {:?}", status);
 }
 
 fn main() {
-    // Main Controller
-    //   - Needs to know all the controllers it can communicate with
-
-    // Controller
-    //   - Needs to know its inputs
-    //   - Needs to know its outputs
-    //   - CAN Bus IDs
-
-    let back_controller = Controller {
-        name: "Back Controller".to_string(),
-    };
+    // TODO(patrik): Compile "the world"
+    // TODO(patrik): Compile "dio"
+    // TODO(patrik): Upload the world to a pico using picoprobe
+    std::fs::create_dir_all("build/the_world").unwrap();
+    generate_cmake_project("build/the_world", "the_world", "the_world");
+    compile_with_make("build/the_world");
 }
