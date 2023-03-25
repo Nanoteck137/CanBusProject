51772b4f5ac540cdf778553344feb88691bde51f
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-03-25 22:18:36 +0100

    The Hand: Testing

--- the_hand/src/main.cpp
+++ the_hand/src/main.cpp
@@ -36,7 +36,7 @@ int main()
 {
     init_system();
 
-    uint outputs[] = {18, 19, 20, PICO_DEFAULT_LED_PIN};
+    uint outputs[] = {16, 17, 18, PICO_DEFAULT_LED_PIN};
     int num_outputs = sizeof(outputs) / sizeof(outputs[0]);
 
     // Initialize the outputs
@@ -55,8 +55,8 @@ int main()
         uint64_t time = 0;
     };
 
-    const size_t NUM_INPUTS = 2;
-    uint inputs[NUM_INPUTS] = {14, 13};
+    const size_t NUM_INPUTS = 6;
+    uint inputs[NUM_INPUTS] = {15, 14, 13, 19, 20, 21};
     InputState input_states[NUM_INPUTS];
 
     for (int i = 0; i < NUM_INPUTS; i++)
@@ -68,62 +68,84 @@ int main()
 
     uint64_t last = time_us_64();
 
+    bool value = false;
+
     while (true)
     {
-        uint64_t current = time_us_64();
-
-        can_frame frame;
-        if (can0.readMessage(&frame) == MCP2515::ERROR_OK)
-        {
-            if (frame.can_id == CONTROL_ID && frame.can_dlc >= 1)
-            {
-                uint8_t controls = frame.data[0];
-
-                gpio_put(outputs[0], (controls & (1 << 0)) > 0);
-                gpio_put(outputs[1], (controls & (1 << 1)) > 0);
-                gpio_put(outputs[2], (controls & (1 << 2)) > 0);
-                gpio_put(outputs[3], (controls & (1 << 3)) > 0);
-            }
-        }
-
         for (int i = 0; i < NUM_INPUTS; i++)
         {
-            InputState* state = input_states + i;
-            state->current_value = gpio_get(inputs[i]);
-            if (state->current_value == 0 &&
-                state->last_value != state->current_value &&
-                (current - state->time) > 200 * 1000)
-            {
-                state->current_toggle_value = !state->current_toggle_value;
-                state->time = current;
-            }
-
-            state->last_value = state->current_value;
+            bool res = gpio_get(inputs[i]);
+            printf("%d, ", res);
         }
 
-        if (current - last > 50 * 1000)
+        printf("\n");
+
+        uint64_t current = time_us_64();
+        if (current - last > 1000 * 1000)
         {
-            uint8_t current_lines = 0;
-            uint8_t current_toggle_lines = 0;
-
-            for (int i = 0; i < NUM_INPUTS; i++)
+            for (int i = 0; i < num_outputs; i++)
             {
-                InputState* state = input_states + i;
-                current_lines |= ((!state->current_value) << i);
-                current_toggle_lines |= (state->current_toggle_value << i);
+                printf("Toggle\n");
+                gpio_put(outputs[i], value);
             }
-
-            // printf("Sending Line Message\n");
-            can_frame send;
-            send.can_id = LINE_ID;
-            send.can_dlc = 2;
-            send.data[0] = current_lines;        // CURRENT VALUES
-            send.data[1] = current_toggle_lines; // TOGGLED VALUES
-            if (can0.sendMessage(&send) != MCP2515::ERROR_OK)
-            {
-                printf("Error?\n");
-            }
+            value = !value;
             last = current;
         }
+
+        // uint64_t current = time_us_64();
+        //
+        // can_frame frame;
+        // if (can0.readMessage(&frame) == MCP2515::ERROR_OK)
+        // {
+        //     if (frame.can_id == CONTROL_ID && frame.can_dlc >= 1)
+        //     {
+        //         uint8_t controls = frame.data[0];
+        //
+        //         gpio_put(outputs[0], (controls & (1 << 0)) > 0);
+        //         gpio_put(outputs[1], (controls & (1 << 1)) > 0);
+        //         gpio_put(outputs[2], (controls & (1 << 2)) > 0);
+        //         gpio_put(outputs[3], (controls & (1 << 3)) > 0);
+        //     }
+        // }
+        //
+        // for (int i = 0; i < NUM_INPUTS; i++)
+        // {
+        //     InputState* state = input_states + i;
+        //     state->current_value = gpio_get(inputs[i]);
+        //     if (state->current_value == 0 &&
+        //         state->last_value != state->current_value &&
+        //         (current - state->time) > 200 * 1000)
+        //     {
+        //         state->current_toggle_value = !state->current_toggle_value;
+        //         state->time = current;
+        //     }
+        //
+        //     state->last_value = state->current_value;
+        // }
+        //
+        // if (current - last > 50 * 1000)
+        // {
+        //     uint8_t current_lines = 0;
+        //     uint8_t current_toggle_lines = 0;
+        //
+        //     for (int i = 0; i < NUM_INPUTS; i++)
+        //     {
+        //         InputState* state = input_states + i;
+        //         current_lines |= ((!state->current_value) << i);
+        //         current_toggle_lines |= (state->current_toggle_value << i);
+        //     }
+        //
+        //     // printf("Sending Line Message\n");
+        //     can_frame send;
+        //     send.can_id = LINE_ID;
+        //     send.can_dlc = 2;
+        //     send.data[0] = current_lines;        // CURRENT VALUES
+        //     send.data[1] = current_toggle_lines; // TOGGLED VALUES
+        //     if (can0.sendMessage(&send) != MCP2515::ERROR_OK)
+        //     {
+        //         printf("Error?\n");
+        //     }
+        //     last = current;
+        // }
     }
 }
