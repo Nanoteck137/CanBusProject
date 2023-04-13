3149444b83f4bdece5b5c2b4f48e9333fee52bce
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-04-13 22:10:19 +0200

    Silver Chariot: Changed Identify to the new spec and working on the user input

--- silver_chariot/src/main.rs
+++ silver_chariot/src/main.rs
@@ -1,4 +1,5 @@
-use std::io::{ErrorKind, Read, Write};
+use std::collections::HashMap;
+use std::io::{BufRead, ErrorKind, Read, Write};
 use std::os::unix::net::{UnixListener, UnixStream};
 use std::path::Path;
 
@@ -82,16 +83,25 @@ where
     Some(())
 }
 
+fn make_version(major: u8, minor: u8, patch: u8) -> u16 {
+    ((major & 0x3f) as u16) << 10 |
+        ((minor & 0x3f) as u16) << 4 |
+        (patch & 0xf) as u16
+}
+
 fn handle_identify<W>(writer: &mut W, packet: &Packet)
 where
     W: Write,
 {
-    let mut data = [0; 35];
-    data[0] = 0; // VERSION
-    data[1] = 0; // VERSION
-    data[3] = 0; // NUM_CMDS
+    let mut data = Vec::new();
+
+    let version = make_version(2, 1, 1);
+    data.extend_from_slice(&version.to_le_bytes());
+    data.push(10); // NUM_CMDS
+
     let name = "Testing";
-    data[3..3 + name.len()].copy_from_slice(name.as_bytes());
+    data.push(name.len() as u8);
+    data.extend_from_slice(name.as_bytes());
 
     write_response_packet(writer, packet.pid, ErrorCode::Success, &data);
 }
@@ -162,19 +172,103 @@ fn handle_connection(mut stream: UnixStream) {
     }
 }
 
-fn main() {
-    let path = Path::new("/tmp/silver_chariot.sock");
+#[derive(Debug)]
+enum Command {
+    SetControl { name: String, val: bool },
+}
 
-    if path.exists() {
-        std::fs::remove_file(&path).unwrap();
+fn parse_command(cmd: &str) -> Option<Command> {
+    let mut split = cmd.split(' ');
+    let cmd = split.next()?;
+    let cmd = cmd.to_lowercase();
+    let cmd = cmd.as_str();
+
+    let params = split.collect::<Vec<&str>>();
+
+    match cmd {
+        "control" => {
+            if params.len() < 2 {
+                return None;
+            }
+
+            let name = params[0].to_string();
+            let val = params[1];
+            let val = match val {
+                "on" => true,
+                "off" => false,
+                _ => return None,
+            };
+
+            Some(Command::SetControl { name, val })
+        }
+        _ => None,
     }
+}
 
-    let listener = UnixListener::bind(path).unwrap();
+#[derive(Default, Debug)]
+struct State {
+    trunk_lamp: bool,
+    backup_camera: bool,
+    backup_lights: bool,
+}
 
-    loop {
-        match listener.accept() {
-            Ok((stream, _addr)) => handle_connection(stream),
-            Err(e) => eprintln!("Error: {:?}", e),
+impl State {
+    fn set_control(&mut self, name: &str, val: bool) -> Option<()> {
+        match name {
+            "trunk_lamp" => {
+                self.trunk_lamp = val;
+                Some(())
+            }
+
+            "backup_camera" => {
+                self.backup_camera = val;
+                Some(())
+            }
+
+            "backup_lights" => {
+                self.backup_lights = val;
+                Some(())
+            }
+
+            _ => None,
         }
     }
+}
+
+fn main() {
+    let mut state = State::default();
+
+    let stdin = std::io::stdin();
+
+    let mut line = String::new();
+    stdin.lock().read_line(&mut line).unwrap();
+
+    let line = line.trim();
+    println!("Line: {:?}", line.trim());
+
+    let cmd = parse_command(line).expect("Failed to parse command");
+    println!("Cmd: {:?}", cmd);
+
+    match cmd {
+        Command::SetControl { name, val } => {
+            state.set_control(&name, val).expect("No control");
+        }
+    }
+
+    println!("State: {:?}", state);
+
+    // let path = Path::new("/tmp/silver_chariot.sock");
+    //
+    // if path.exists() {
+    //     std::fs::remove_file(&path).unwrap();
+    // }
+    //
+    // let listener = UnixListener::bind(path).unwrap();
+    //
+    // loop {
+    //     match listener.accept() {
+    //         Ok((stream, _addr)) => handle_connection(stream),
+    //         Err(e) => eprintln!("Error: {:?}", e),
+    //     }
+    // }
 }
